65edf72c04cab805434c1b4c0f6a9d8e9d3fb00e
Aaron Morse <39176202+Aaron-Morse@users.noreply.github.com>
2022-04-24 16:35:18 -0700

    Add files via upload

--- intro-section-with-dropdown-navigation-main/app.js
+++ intro-section-with-dropdown-navigation-main/app.js
@@ -1,6 +1,8 @@
 // Page elements
 const mobileNavBtn = document.querySelector('#mobile-nav');
 const mobileNavCloseBtn = document.querySelector('#mobile-nav-close-btn');
+const featuresDropdown = document.querySelector('#features');
+const companyDropdown = document.querySelector('#company');
 
 // Functions
 function mobileMenuToggle(input) {
@@ -13,6 +15,27 @@ function mobileMenuToggle(input) {
     }
 }
 
+function mobileMenuItemsToggle(input) {
+    if (input.style.display === '' || input.style.display === 'none') {
+        input.style.display = 'block';
+        input.previousElementSibling.style.transform = 'rotate(360deg)';
+    } else {
+        input.style.display = 'none';
+        input.previousElementSibling.style.transform = 'rotate(180deg)';
+    }
+}
+
 // Event listeners
 mobileNavBtn.addEventListener('click', (event) => mobileMenuToggle(event.target));
+
 mobileNavCloseBtn.addEventListener('click', (event) => mobileMenuToggle(event.target));
+
+featuresDropdown.addEventListener('click', (event) => {
+    const ul = featuresDropdown.querySelector('ul');
+    mobileMenuItemsToggle(ul);
+});
+
+companyDropdown.addEventListener('click', (event) => {
+    const ul = companyDropdown.querySelector('ul');
+    mobileMenuItemsToggle(ul);
+});
