6c5bd3192ad0e2598a610d82d06b92e49e3e63a4
Aaron Morse <39176202+Aaron-Morse@users.noreply.github.com>
2022-07-03 19:45:45 -0700

    Add files via upload

--- your-turn-dynamic-twitter/index.js
+++ your-turn-dynamic-twitter/index.js
@@ -48,55 +48,9 @@ const users = {
     }
 };
 
-const user1 = {
-    userName: '@elonmusk',
-    displayName: 'Elon Musk',
-    joinedDate: 'June 2009',
-    followingCount: 103,
-    followerCount: 47900000,
-    avatarURL: 'assets/elonmusk.jpg',
-    coverPhotoURL: 'assets/elonmusk-cover.jpeg',
-    tweets: [
-        {
-            text: 'I admit to judging books by their cover',
-            timestamp: '2/10/2021 00:01:20'
-        },
-        {
-            text: 'Starship to the moon',
-            timestamp: '2/09/2021 18:37:12'
-        },
-        {
-            text: 'Out on launch pad, engine swap underway',
-            timestamp: '2/09/2021 12:11:51'
-        }
-    ]
-};
 
-const user2 = {
-    userName: '@BillGates',
-    displayName: 'Bill Gates',
-    joinedDate: 'June 2009',
-    followingCount: 274,
-    followerCount: 53800000,
-    avatarURL: 'assets/billgates.jpg',
-    coverPhotoURL: 'assets/billgates-cover.jpeg',
-    tweets: [
-        {
-            text: 'Everybody asks, how is the next Windows coming along? But nobody asks how is Bill? :/',
-            timestamp: '2/10/2021 00:01:20'
-        },
-        {
-            text: 'Should I start tweeting memes? Let me know in a comment.',
-            timestamp: '2/09/2021 18:37:12'
-        },
-        {
-            text: 'In 2020, I read a book every hour.',
-            timestamp: '2/09/2021 12:11:51'
-        }
-    ]
-};
-
-const user = user1;
+const urlParam = new URLSearchParams(location.search);
+const user = users[urlParam.get('user')] || users.user1;
 const displayNames = document.querySelectorAll('.display-name');
 const tweetCount = document.querySelector('.tweet-count');
 const coverPhoto = document.querySelector('.cover-photo');
@@ -149,19 +103,6 @@ function updateUser(user) {
 }
 
 updateUser(user);
-
-// Listens for enter key event and changes the contents of the page based on the query param
-window.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-        const urlParams = new URLSearchParams(location.search);
-        for (const [key, value] of urlParams) {
-            if (users[value]) {
-                tweetsSection.innerHTML = '';
-                updateUser(users[value]);
-            }
-        }
-    }
-});
 
 
 
